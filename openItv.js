154739372b389b26cb09c8f5e4d72637854a8750
Mike Ralphson <mike.ralphson@gmail.com>
2016-08-06 16:16:27 +0100

    Update descriptions, openapi2js

--- openItv.js
+++ openItv.js
@@ -1,8 +1,8 @@
 const commonScreensize = 'screensize';
 const commonBroadcaster = 'broadcaster';
 
-/* No description */
-function getApiepisodeProgramme(target,platform,programmeid){
+/* Get Mercury programme details */
+function getApiEpisodeProgramme(target,platform,programmeid){
   var p = '/api/{target}/{platform}/Episode/Programme/{programmeId}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -10,8 +10,8 @@ function getApiepisodeProgramme(target,platform,programmeid){
   return p;
 }
 
-/* No description */
-function getApiepisodeBydate(target,platform,year,month,day){
+/* Get Mercury Episode by Date */
+function getApiEpisodeBydate(target,platform,year,month,day){
   var p = '/api/{target}/{platform}/Episode/ByDate/{year}/{month}/{day}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -21,8 +21,8 @@ function getApiepisodeBydate(target,platform,year,month,day){
   return p;
 }
 
-/* No description */
-function getApiprogrammePerchannelAd(target,platform,channelname,resultcount){
+/* Get Mercury audio-described (limited) */
+function getApiProgrammePerchannelAd(target,platform,channelname,resultcount){
   var p = '/api/{target}/{platform}/programme/perchannel/AD/{channelName}/{resultCount}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -31,8 +31,8 @@ function getApiprogrammePerchannelAd(target,platform,channelname,resultcount){
   return p;
 }
 
-/* No description */
-function getApiprogrammeSearch(target,platform,searchterm){
+/* Get Mercury search */
+function getApiProgrammeSearch(target,platform,searchterm){
   var p = '/api/{target}/{platform}/Programme/Search/{searchTerm}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -40,16 +40,16 @@ function getApiprogrammeSearch(target,platform,searchterm){
   return p;
 }
 
-/* No description */
-function getApiepisodeMostwatched(target,platform){
+/* Get Mercury most-watched */
+function getApiEpisodeMostwatched(target,platform){
   var p = '/api/{target}/{platform}/Episode/MostWatched';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApiprogrammePerchannel(target,platform,channelname){
+/* Get Mercury per channel */
+function getApiProgrammePerchannel(target,platform,channelname){
   var p = '/api/{target}/{platform}/Programme/PerChannel/{channelName}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -57,8 +57,8 @@ function getApiprogrammePerchannel(target,platform,channelname){
   return p;
 }
 
-/* No description */
-function getApiepisodeIndex(target,platform,episodeid){
+/* Get Mercury episode index */
+function getApiEpisodeIndex(target,platform,episodeid){
   var p = '/api/{target}/{platform}/Episode/index/{episodeId}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -66,16 +66,16 @@ function getApiepisodeIndex(target,platform,episodeid){
   return p;
 }
 
-/* No description */
-function getApiwhatsonscheduleToday(target,platform){
+/* Get Mercury schedule today */
+function getApiWhatsonscheduleToday(target,platform){
   var p = '/api/{target}/{platform}/whatsonschedule/today';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApiprogrammePerchannelAd2(target,platform,channelname){
+/* Get Mercury audio-described */
+function getApiProgrammePerchannelAd2(target,platform,channelname){
   var p = '/api/{target}/{platform}/programme/perchannel/AD/{channelName}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -83,11 +83,11 @@ function getApiprogrammePerchannelAd2(target,platform,channelname){
   return p;
 }
 
-/* No description */
+/* Get Mercury error log digest */
 const getErrorlogAspxDigestrss = '//errorlog.aspx/digestrss';
 
-/* No description */
-function getApiepisodeProgramme2(target,platform,episodeid){
+/* Get Mercury episode/programme */
+function getApiEpisodeProgramme2(target,platform,episodeid){
   var p = '/api/{target}/{platform}/episode/programme/{episodeId}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -95,7 +95,7 @@ function getApiepisodeProgramme2(target,platform,episodeid){
   return p;
 }
 
-/* No description */
+/* Get Mercury Postcode */
 function getApiPostcode(target,postcode){
   var p = '/api/{target}/postcode/{postcode}';
   p = p.replace('{target}',target);
@@ -103,8 +103,8 @@ function getApiPostcode(target,postcode){
   return p;
 }
 
-/* No description */
-function getApiwhatsonschedule(target,platform,date){
+/* Get Mercury schedule by date */
+function getApiWhatsonschedule(target,platform,date){
   var p = '/api/{target}/{platform}/whatsonschedule/{date}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -112,11 +112,11 @@ function getApiwhatsonschedule(target,platform,date){
   return p;
 }
 
-/* No description */
+/* Get Mercury error log download */
 const getErrorlogAspxDownload = '//errorlog.aspx/download';
 
-/* No description */
-function getApiprogrammeIndex(target,platform,programmeid){
+/* Get Mercury programme index */
+function getApiProgrammeIndex(target,platform,programmeid){
   var p = '/api/{target}/{platform}/programme/index/{programmeId}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -124,51 +124,51 @@ function getApiprogrammeIndex(target,platform,programmeid){
   return p;
 }
 
-/* No description */
-function getApiepisodeCrucial(target,platform){
+/* Get Mercury crucial */
+function getApiEpisodeCrucial(target,platform){
   var p = '/api/{target}/{platform}/Episode/Crucial';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApiwhatsonscheduleYesterday(target,platform){
+/* Get Mercury schedule yesterday */
+function getApiWhatsonscheduleYesterday(target,platform){
   var p = '/api/{target}/{platform}/whatsonschedule/yesterday';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
+/* Get Mercury error log */
 const getErrorlogAspxRss = '//errorlog.aspx/rss';
 
-/* No description */
-function getApischedule(target,platform){
+/* Get Mercury current schedule */
+function getApiSchedule(target,platform){
   var p = '/api/{target}/{platform}/schedule';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApilastweek(target,platform){
+/* Get Mercury lastweek */
+function getApiLastweek(target,platform){
   var p = '/api/{target}/{platform}/lastweek';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApiepisodeTopten(target,platform){
+/* Get Mercury top ten */
+function getApiEpisodeTopten(target,platform){
   var p = '/api/{target}/{platform}/Episode/TopTen';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
-function getApiprogrammePerchannel2(target,platform,channelname,resultcount){
+/* Get Mercury per channel (limited) */
+function getApiProgrammePerchannel2(target,platform,channelname,resultcount){
   var p = '/api/{target}/{platform}/Programme/PerChannel/{channelName}/{resultCount}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -177,8 +177,8 @@ function getApiprogrammePerchannel2(target,platform,channelname,resultcount){
   return p;
 }
 
-/* No description */
-function getApiprogrammeSearchatoz(target,platform,characterfilter){
+/* Get Mercury Search AtoZ */
+function getApiProgrammeSearchatoz(target,platform,characterfilter){
   var p = '/api/{target}/{platform}/Programme/SearchAtoZ/{characterFilter}';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
@@ -186,7 +186,7 @@ function getApiprogrammeSearchatoz(target,platform,characterfilter){
   return p;
 }
 
-/* No description */
+/* Get Mercury Menu */
 function getApi(target,platform){
   var p = '/api/{target}/{platform}';
   p = p.replace('{target}',target);
@@ -194,7 +194,7 @@ function getApi(target,platform){
   return p;
 }
 
-/* No description */
+/* Get Mercury image by programme id */
 function getProgrammeImageJpg(platform,programmeid){
   var p = '/{platform}/programme/{programmeId}/image.jpg';
   p = p.replace('{platform}',platform);
@@ -204,22 +204,22 @@ function getProgrammeImageJpg(platform,programmeid){
 const getProgrammeImageJpgW = 'w';
 const getProgrammeImageJpgH = 'h';
 
-/* No description */
+/* Get Mercury BBC Interlinking */
 function getLinking(yyyymmdd){
   var p = '/linking/{yyyymmdd}';
   p = p.replace('{yyyymmdd}',yyyymmdd);
   return p;
 }
 
-/* No description */
-function getApiepisodeCarousel(target,platform){
+/* Get Mercury carousel */
+function getApiEpisodeCarousel(target,platform){
   var p = '/api/{target}/{platform}/Episode/Carousel';
   p = p.replace('{target}',target);
   p = p.replace('{platform}',platform);
   return p;
 }
 
-/* No description */
+/* Get Mercury image by episodeId */
 function getEpisodeImageJpg(platform,episodeid){
   var p = '/{platform}/episode/{episodeId}/image.jpg';
   p = p.replace('{platform}',platform);
@@ -229,7 +229,7 @@ function getEpisodeImageJpg(platform,episodeid){
 const getEpisodeImageJpgW = 'w';
 const getEpisodeImageJpgH = 'h';
 
-/* No description */
+/* Get Mercury Reverse Geocode */
 function getApiGeoReversegeolookup(lat,long){
   var p = '/api/geo/reversegeolookup/{lat}/{long}';
   p = p.replace('{lat}',lat);
@@ -240,33 +240,33 @@ function getApiGeoReversegeolookup(lat,long){
 module.exports = {
   commonScreensize : commonScreensize,
   commonBroadcaster : commonBroadcaster,
-  getApiepisodeProgramme : getApiepisodeProgramme,
-  getApiepisodeBydate : getApiepisodeBydate,
-  getApiprogrammePerchannelAd : getApiprogrammePerchannelAd,
-  getApiprogrammeSearch : getApiprogrammeSearch,
-  getApiepisodeMostwatched : getApiepisodeMostwatched,
-  getApiprogrammePerchannel : getApiprogrammePerchannel,
-  getApiepisodeIndex : getApiepisodeIndex,
-  getApiwhatsonscheduleToday : getApiwhatsonscheduleToday,
-  getApiprogrammePerchannelAd2 : getApiprogrammePerchannelAd2,
+  getApiEpisodeProgramme : getApiEpisodeProgramme,
+  getApiEpisodeBydate : getApiEpisodeBydate,
+  getApiProgrammePerchannelAd : getApiProgrammePerchannelAd,
+  getApiProgrammeSearch : getApiProgrammeSearch,
+  getApiEpisodeMostwatched : getApiEpisodeMostwatched,
+  getApiProgrammePerchannel : getApiProgrammePerchannel,
+  getApiEpisodeIndex : getApiEpisodeIndex,
+  getApiWhatsonscheduleToday : getApiWhatsonscheduleToday,
+  getApiProgrammePerchannelAd2 : getApiProgrammePerchannelAd2,
   getErrorlogAspxDigestrss : getErrorlogAspxDigestrss,
-  getApiepisodeProgramme2 : getApiepisodeProgramme2,
+  getApiEpisodeProgramme2 : getApiEpisodeProgramme2,
   getApiPostcode : getApiPostcode,
-  getApiwhatsonschedule : getApiwhatsonschedule,
+  getApiWhatsonschedule : getApiWhatsonschedule,
   getErrorlogAspxDownload : getErrorlogAspxDownload,
-  getApiprogrammeIndex : getApiprogrammeIndex,
-  getApiepisodeCrucial : getApiepisodeCrucial,
-  getApiwhatsonscheduleYesterday : getApiwhatsonscheduleYesterday,
+  getApiProgrammeIndex : getApiProgrammeIndex,
+  getApiEpisodeCrucial : getApiEpisodeCrucial,
+  getApiWhatsonscheduleYesterday : getApiWhatsonscheduleYesterday,
   getErrorlogAspxRss : getErrorlogAspxRss,
-  getApischedule : getApischedule,
-  getApilastweek : getApilastweek,
-  getApiepisodeTopten : getApiepisodeTopten,
-  getApiprogrammePerchannel2 : getApiprogrammePerchannel2,
-  getApiprogrammeSearchatoz : getApiprogrammeSearchatoz,
+  getApiSchedule : getApiSchedule,
+  getApiLastweek : getApiLastweek,
+  getApiEpisodeTopten : getApiEpisodeTopten,
+  getApiProgrammePerchannel2 : getApiProgrammePerchannel2,
+  getApiProgrammeSearchatoz : getApiProgrammeSearchatoz,
   getApi : getApi,
   getProgrammeImageJpg : getProgrammeImageJpg,
   getLinking : getLinking,
-  getApiepisodeCarousel : getApiepisodeCarousel,
+  getApiEpisodeCarousel : getApiEpisodeCarousel,
   getEpisodeImageJpg : getEpisodeImageJpg,
   getApiGeoReversegeolookup : getApiGeoReversegeolookup,
   host : 'mercury.itv.com'
